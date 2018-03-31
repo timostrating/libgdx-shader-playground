2942a708879959795985654fc3a57cba1d7a71fe
Timo Strating <timostrating@home.nl>
2018-03-31 19:47:38 +0200

    added a basic fragment and vertex shader

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,16 +4,28 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Sprite sprite;
+	String vertexShader;
+	String fragmentShader;
+	ShaderProgram shaderProgram;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(img);
+		sprite.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		vertexShader = Gdx.files.internal("vertex.glsl").readString();
+		fragmentShader = Gdx.files.internal("fragment.glsl").readString();
+		shaderProgram = new ShaderProgram(vertexShader,fragmentShader);
 	}
 
 	@Override
@@ -21,13 +33,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.setShader(shaderProgram);
+		batch.draw(sprite,sprite.getX(),sprite.getY(),sprite.getWidth(),sprite.getHeight());
 		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
